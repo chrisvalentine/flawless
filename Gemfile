38c3894adfde40553261892feb20e48e80199906
chrisvalentine <chrisvalentinemail@gmail.com>
2012-09-09 22:48:53 -0700

    some work on calendar to view dates, other tweaks

--- Gemfile
+++ Gemfile
@@ -1,10 +1,13 @@
 source :rubygems
 
-gem 'sinatra', '>= 1.0'
+gem 'sinatra'
 gem 'rake'
 gem 'data_mapper'
 gem 'dm-core'
 gem 'haml'
 gem 'thin'
+gem 'sinatra-reloader'
+gem 'sqlite3'
+gem 'dm-sqlite-adapter'
 
 
--- flawless.rb
+++ flawless.rb
@@ -3,6 +3,7 @@
 #
 #
 #
+require 'bundler/setup'
 require 'sinatra'
 require 'data_mapper'
 require 'haml'
@@ -11,6 +12,48 @@ require 'sinatra/reloader'
 DataMapper::setup(:default,"sqlite3://#{Dir.pwd}/flawless.db")
 
 set :views, File.dirname(__FILE__) + "/views"
+
+class Date
+  def first_of_month
+    Date.new(year, month, 1)
+  end
+  
+  def first_sunday_of_first_week
+    first = first_of_month
+    first - first.wday
+  end
+  
+  def last_of_month
+	Date.new(self.year, self.month + 1, 1) - 1
+  end
+  
+  def last_saturday_of_last_week
+    last = last_of_month
+	last + (6 - last.wday)
+  end  
+end
+
+class Array
+         def in_groups_of(number, fill_with = nil)
+           if fill_with == false
+             collection = self
+           else
+             # size % number gives how many extra we have;
+             # subtracting from number gives how many to add;
+             # modulo number ensures we don't add group of just fill.
+             padding = (number - size % number) % number
+             collection = dup.concat([fill_with] * padding)
+           end
+ 
+           if block_given?
+             collection.each_slice(number) { |slice| yield(slice) }
+           else
+             returning [] do |groups|
+               collection.each_slice(number) { |group| groups << group }
+             end
+           end
+         end
+end
 
 class Entry
   include DataMapper::Resource
@@ -40,4 +83,10 @@ end
 
 get '/entry/:id' do
   haml :entry
+end
+
+get '/calendar' do
+	@dates = (Date.today.first_sunday_of_first_week..Date.today.last_saturday_of_last_week)
+	@month = Date.today.month
+	haml :calendar
 end
